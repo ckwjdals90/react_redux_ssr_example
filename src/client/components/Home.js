ca126fcf6b632633ec4f415274ce9d671ae015f4
Jungmin Cha <ckwjdals90@gmail.com>
2017-10-25 14:50:55 -0700

    configuring the public bundle.js for client side rendering

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div>
       <div> I'm the Home component </div>
+      <button onClick={() => console.log('Hi, there!')}> Press Me! </button>
     </div>
   );
 };
